f548c18e20d0d01b7a19e30100d6dafe36b1520c
happyyeon <happyeon@kakao.com>
2023-06-19 16:15:25 +0900

    secret key hide

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e4hbxd8(20-*&t(e8e_#kj!rv9lr_s&7h$$!+bc_yvhv+&_ta^"
+import json
+with open('secrets.json') as f:
+    secrets = json.loads(f.read())
+
+SECRET_KEY = secrets['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
