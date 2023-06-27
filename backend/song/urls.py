0ef6d9ed6922425391a4536942cfe6618f155deb
jjh <wlsgur222@naver.com>
2023-06-28 08:53:41 +0900

    노래 리스트에서 마이리스트 추가 기능 구현 ~ing

--- backend/song/urls.py
+++ backend/song/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import song_list, search_view
+from .views import song_list, search_view, add_to_database
 
 app_name = 'song'
 
 urlpatterns = [
     path('', song_list, name='song-list'),
     path('search/', search_view, name='song-search'),
+    path('add-to-database/', add_to_database, name='add_to_database'),
 ]
