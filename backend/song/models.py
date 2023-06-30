4f665691a7a048ae8096bfc4591026cbe345ac1e
jjh <wlsgur222@naver.com>
2023-06-30 14:39:23 +0900

    검색한 노래 mylist 추가 기능 구현

--- backend/song/models.py
+++ backend/song/models.py
@@ -47,18 +47,18 @@ class Song(models.Model):
 class Ky_pop(models.Model):
     rank = models.IntegerField()
     title = models.CharField(max_length=255)
-    artist = models.CharField(max_length=30)
-    cmp = models.CharField(max_length=30)
-    writer = models.CharField(max_length=30)
-    ky_number = models.ForeignKey(Kysong, on_delete=models.CASCADE, related_name='kypop_set', to_field='song_num')
+    artist = models.CharField(max_length=255)
+    cmp = models.CharField(max_length=30, null=True)
+    writer = models.CharField(max_length=30, null=True)
+    song_num = models.ForeignKey(Kysong, on_delete=models.CASCADE, related_name='kypop_set', to_field='song_num')
 
 
 #태진 인기 순위
 class Tj_pop(models.Model):
     rank = models.IntegerField()
     title = models.CharField(max_length=255)
-    artist = models.CharField(max_length=30)
-    cmp = models.CharField(max_length=30)
-    writer = models.CharField(max_length=30)
-    tj_number = models.ForeignKey(Tjsong, on_delete=models.CASCADE, related_name='tjpop_set', to_field='song_num')
+    artist = models.CharField(max_length=255)
+    cmp = models.CharField(max_length=30, null=True)
+    writer = models.CharField(max_length=30, null=True)
+    song_num = models.ForeignKey(Tjsong, on_delete=models.CASCADE, related_name='tjpop_set', to_field='song_num')
 
