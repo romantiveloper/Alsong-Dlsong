f58e08a43b59bba8d5c4a1da4a6c68678e85066f
jjh <wlsgur222@naver.com>
2023-07-03 09:34:43 +0900

    마이리스트 중복노래추가x

--- backend/song/views.py
+++ backend/song/views.py
@@ -6,7 +6,9 @@ from django.db.models import Q
 from django.http import JsonResponse
 from mylist.models import Mylist, Myfolder
 import json
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required   # 로그인한 사용자 정보
+from django.core.exceptions import ValidationError  # 중복 데이터 에러 처리
+from rest_framework.decorators import api_view
 # Create your views here.
 
 def song_list(request):
@@ -54,29 +56,45 @@ def search_view(request):
     data = {'results': results, 'folders':folders}
     return render(request, 'songlist/search.html', data)
 
-
+@api_view(['POST'])
 @login_required
 def add_to_database(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-
-        # 현재 로그인한 사용자 정보 가져오기
         user = request.user
-
-        # 선택한 My_folder의 list_number와 list_name 가져오기
         list_number = data['listNumber']
         list_name = data['listName']
+        title = data['title']
+        artist = data['artist']
+        ky_number = data['kySongNum']
+        tj_number = data['tjSongNum']
 
-        # 데이터베이스에 값을 추가하는 로직
+        # 중복된 데이터 확인
+        duplicate_records = Mylist.objects.filter(
+            list_name=list_name,
+            title=title,
+            artist=artist,
+            ky_number_id=ky_number,
+            tj_number_id=tj_number,
+            list_number_id=list_number,
+            user=user
+        )
+
+        if duplicate_records.exists():
+            return Response(status=500, data=dict(message='이미 추가된 노래입니다.'))
+
+        # 중복이 없을 경우 데이터베이스에 추가
+        cmp = data.get('cmp', '')
+        writer = data.get('writer', '')
         mylist = Mylist(
             list_name=list_name,
             user=user,
-            title=data['title'],
-            artist=data['artist'],
-            cmp=data['cmp'],
-            writer=data['writer'],
-            ky_number_id=data['kySongNum'],
-            tj_number_id=data['tjSongNum'],
+            title=title,
+            artist=artist,
+            cmp=cmp,
+            writer=writer,
+            ky_number_id=ky_number,
+            tj_number_id=tj_number,
             list_number_id=list_number
         )
         mylist.save()
