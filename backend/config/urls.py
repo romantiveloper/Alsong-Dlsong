a5c730a3984bb0de4c73bfb46040870292ec6eac
jjh <wlsgur222@naver.com>
2023-07-11 19:50:13 +0900

    카카오 로그인 유지되지 않던 문제 해결

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,36 +1,11 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from .views import UserViewSet
 
-router = routers.DefaultRouter()
-router.register('user', UserViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('mylist.urls')),
-    path('', include(router.urls)),
     path("song-list/", include('song.urls')),
     path("user/", include('user.urls')),
-    path('accounts/', include('dj_rest_auth.urls')),
-    path('accounts/', include('dj_rest_auth.registration.urls')),
-    path("accounts/", include('allauth.urls')),
-    #path('accounts/', include('user.urls')),
 ]
 
